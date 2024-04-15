8d5a077501d1b6d75f9688e8f63df50309064778
최진우 <wlsdn1536@naver.com>
2024-04-16 08:39:23 +0900

    Delete BluetoothAlarmClock directory

--- BluetoothAlarmClock/Core/Inc/flash.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FLASH_H
-#define __FLASH_H
-
-#include "stm32f4xx_hal.h"
-
-HAL_StatusTypeDef FLASH_Write(uint32_t address, uint32_t *data, uint32_t size);
-HAL_StatusTypeDef FLASH_Read(uint32_t address, uint32_t *data, uint32_t size);
-
-#endif /* __FLASH_H */
